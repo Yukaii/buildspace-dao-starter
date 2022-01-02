44bedbf001b39757b61bd3b0b44c40e6cace36dd
Yukai Huang <yukaihuangtw@gmail.com>
2022-01-02 16:24:27 +0800

    Revoke QQ

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,26 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x4aB847CcdA81A7Ec3B7Fe9edca9D998Efba542f4",
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
