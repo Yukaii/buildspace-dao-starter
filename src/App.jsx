99eb9899efaef0057115cc191531b31eb6eaa4b2
Yukai Huang <yukaihuangtw@gmail.com>
2022-01-02 16:24:31 +0800

    show error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
+import { UnsupportedChainIdError } from "@web3-react/core";
 
 // import thirdweb
 import { useWeb3 } from "@3rdweb/hooks";
@@ -178,6 +179,18 @@ const App = () => {
       };
     });
   }, [memberAddresses, memberTokenAmounts]);
+
+  if (error instanceof UnsupportedChainIdError ) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
